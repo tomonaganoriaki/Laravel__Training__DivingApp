c2b6c38961b7bdcda554cf641649861192e60851
tomonaganoriaki <tomonaganoriaki@gmail.com>
2023-11-09 01:16:39 +0900

    マイグレーションファイル作成＆マイグレーション済

---
+++ database/migrations/2023_11_08_203550_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('order_number');
+            $table->string('status');
+            $table->integer('total_price');
+            $table->string('payment_method');
+            $table->dateTime('order_date');
+            $table->dateTime('shipped_date');
+            $table->timestamps();
+            $table->softDeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
