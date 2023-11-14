4810d35bbe5cebd954b51d1e2caa230c177c9b72
tomonaganoriaki <tomonaganoriaki@gmail.com>
2023-11-14 17:20:53 +0900

    ルーティングに入る値の制御追加＋useの順番をアルファベット順に変更

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Tag;
+use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
--- routes/admin.php
+++ routes/admin.php
@@ -34,48 +34,48 @@ Route::get('/dashboard', function () {
 })->middleware(['auth:admin'])->name('admin.dashboard');
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredAdminController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredAdminController::class, 'store']);
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+    Route::get('register', [RegisteredAdminController::class, 'create'])->name('register')->where('id', '[0-9]+');
+    Route::post('register', [RegisteredAdminController::class, 'store'])->where('id', '[0-9]+');
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login')->where('id', '[0-9]+');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->where('id', '[0-9]+');
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request')->where('id', '[0-9]+');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email')->where('id', '[0-9]+');
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset')->where('id', '[0-9]+');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store')->where('id', '[0-9]+');
 });
 
 Route::middleware('auth:admin')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware('throttle:6,1')->name('verification.send');
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice')->where('id', '[0-9]+');
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify')->where('id', '[0-9]+');
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware('throttle:6,1')->name('verification.send')->where('id', '[0-9]+');
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm')->where('id', '[0-9]+');
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store'])->where('id', '[0-9]+');
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update')->where('id', '[0-9]+');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout')->where('id', '[0-9]+');
     
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')->where('id', '[0-9]+');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update')->where('id', '[0-9]+');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy')->where('id', '[0-9]+');
 
-    Route::get('/account', [AdminAccountController::class, 'index'])->name('account.index');
-    Route::get('/account/create', [AdminAccountController::class, 'create'])->name('account.create');
-    Route::post('/account', [AdminAccountController::class, 'store'])->name('account.store');
-    Route::get('/account/{id}/edit', [AdminAccountController::class, 'edit'])->name('account.edit');
-    Route::post('/account/{id}', [AdminAccountController::class, 'update'])->name('account.update');
-    Route::delete('/account/{id}', [AdminAccountController::class, 'destroy'])->name('account.destroy');
+    Route::get('/account', [AdminAccountController::class, 'index'])->name('account.index')->where('id', '[0-9]+');
+    Route::get('/account/create', [AdminAccountController::class, 'create'])->name('account.create')->where('id', '[0-9]+');
+    Route::post('/account', [AdminAccountController::class, 'store'])->name('account.store')->where('id', '[0-9]+');
+    Route::get('/account/{id}/edit', [AdminAccountController::class, 'edit'])->name('account.edit')->where('id', '[0-9]+');
+    Route::post('/account/{id}', [AdminAccountController::class, 'update'])->name('account.update')->where('id', '[0-9]+');
+    Route::delete('/account/{id}', [AdminAccountController::class, 'destroy'])->name('account.destroy')->where('id', '[0-9]+');
 
-    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
-    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-    Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
-    Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
-    Route::post('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
-    Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index')->where('id', '[0-9]+');
+    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create')->where('id', '[0-9]+');
+    Route::post('/category', [CategoryController::class, 'store'])->name('category.store')->where('id', '[0-9]+');
+    Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit')->where('id', '[0-9]+');
+    Route::post('/category/{id}', [CategoryController::class, 'update'])->name('category.update')->where('id', '[0-9]+');
+    Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy')->where('id', '[0-9]+');
 
-    Route::get('/tag', [TagController::class, 'index'])->name('tag.index');
-    Route::get('/tag/create', [TagController::class, 'create'])->name('tag.create');
-    Route::post('/tag', [TagController::class, 'store'])->name('tag.store');
-    Route::get('/tag/{id}/edit', [TagController::class, 'edit'])->name('tag.edit');
-    Route::post('/tag/{id}', [TagController::class, 'update'])->name('tag.update');
-    Route::delete('/tag/{id}', [TagController::class, 'destroy'])->name('tag.destroy');
+    Route::get('/tag', [TagController::class, 'index'])->name('tag.index')->where('id', '[0-9]+');
+    Route::get('/tag/create', [TagController::class, 'create'])->name('tag.create')->where('id', '[0-9]+');
+    Route::post('/tag', [TagController::class, 'store'])->name('tag.store')->where('id', '[0-9]+');
+    Route::get('/tag/{id}/edit', [TagController::class, 'edit'])->name('tag.edit')->where('id', '[0-9]+');
+    Route::post('/tag/{id}', [TagController::class, 'update'])->name('tag.update')->where('id', '[0-9]+');
+    Route::delete('/tag/{id}', [TagController::class, 'destroy'])->name('tag.destroy')->where('id', '[0-9]+');
 });
 
