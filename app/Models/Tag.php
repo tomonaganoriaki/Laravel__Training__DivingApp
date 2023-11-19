dc67a784d78738e9f58174d0e47ac5968fb375a6
tomonaganoriaki <tomonaganoriaki@gmail.com>
2023-11-19 14:28:04 +0900

    モデルファイル修正

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes; 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -15,7 +16,7 @@ class Tag extends Model
     protected $primaryKey = 'id';
     protected $guarded = ['id'];
     
-    public function products()
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
     }
